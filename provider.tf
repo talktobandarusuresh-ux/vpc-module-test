bb286786e5157899959e0c5425f73d702ed820e9
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-06 18:11:00 +0530

    terraform

--- provider.tf
+++ provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "remote-state-86s"
-    key          = "vpc-module-demo"
+    bucket       = "remote-state-sureshbandaru"
+    key          = "module-vpc-test"
     region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
